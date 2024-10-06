66fee1f72b0927298afec113774271248cf20a7d
Shaad <141047163+Shaad98@users.noreply.github.com>
2024-10-06 20:29:07 +0530

    Setter Injections called with Message

--- demo/src/main/java/com/example/Student.java
+++ demo/src/main/java/com/example/Student.java
@@ -1,28 +1,32 @@
 package com.example;
 
 public class Student {
-    public int id;
-    public String name;
-    public String city;
+    private int id;
+    private String name;
+    private String city;
     public int getId() {
         return id;
     }
     public void setId(int id) {
+        System.out.println("Setting Id");
         this.id = id;
     }
     public String getName() {
         return name;
     }
     public void setName(String name) {
+        System.out.println("Setting Name");
         this.name = name;
     }
     public String getCity() {
         return city;
     }
     public void setCity(String city) {
+        System.out.println("Setting City");
         this.city = city;
     }
     public Student(int id, String name, String city) {
+        System.out.println("Constructor is called for Setting Id , Name , City");
         this.id = id;
         this.name = name;
         this.city = city;
